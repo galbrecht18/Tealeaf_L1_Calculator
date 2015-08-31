5e98db25589f914326bd6ad71f291686165e1dcd
galbrecht18 <galbrecht18@gmail.com>
2015-08-30 23:11:30 -0400

    Changes to calculator from TA review and personal review

--- L1_Calculator.rb
+++ L1_Calculator.rb
@@ -8,53 +8,63 @@
 require 'pry'
 
 #Method to get user input
-def get_input msg
+def output_message msg
   puts "***** #{msg} *****"
 end
 
 #Method to handle operations
 def operate num1, num2, oper
 
-  o = case oper
+  case oper
   when 1
     result = num1.to_i + num2.to_i
-    get_input "The solution is #{result}"
+    output_message "The solution is #{result}"
   when 2
     result = num1.to_i - num2.to_i
-    get_input "The solution is #{result}"
+    output_message "The solution is #{result}"
   when 3
     result = num1.to_i * num2.to_i
-    get_input "The solution is #{result}"
+    output_message "The solution is #{result}"
   when 4
-    result = num1.to_f / num2.to_f
-    get_input "The solution is #{result}"
+    begin
+      result = num1.to_f / num2.to_f
+      output_message "The solution is #{result}"
+    rescue 
+      output_message "Error: Cannot Divide by 0!"
+    end
   end
 
 end
 
 #variable to see if the user wants to continue
-continue = 0
+continue = true
 
-while continue == 0 
+while continue == true
 
   #Get the user inputs for first and second numbers
 
-  get_input "Please enter the first number."
+  output_message "Please enter the first number."
   number1 = gets.chomp
 
   #Get second num
-  get_input "Please enter the second number."
+  output_message "Please enter the second number."
   number2 = gets.chomp
 
   #Get operator
-  get_input "Please enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division"
+  output_message "Please enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division"
   operator = gets.chomp.to_i
 
   #Perform the operation
   operate(number1, number2, operator)
 
   #Check if the user wants to continue
-  get_input "Enter 0 to continue, or enter any other number to quit"
-  continue = gets.chomp.to_i
+  output_message "Enter 0 to continue, or enter any other number to quit"
+  continue = gets.chomp
+  if continue == "0" 
+    continue = true 
+  else 
+    continue = false
+    output_message "Thanks for calculating! Exiting now" 
+  end
 
 end
